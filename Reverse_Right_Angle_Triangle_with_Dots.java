1fd86a7143e9aa9d95eded31f4fdbebb9e269f96
DnyaneshKshatriya07 <dnyaneshkshatriya7123@gmail.com>
2023-12-20 11:23:50 +0530

    Reverse_Right_Angle_Triangle_with_Dots

--- Reverse_Right_Angle_Triangle_with_Dots.java
+++ Reverse_Right_Angle_Triangle_with_Dots.java
@@ -1,16 +1,16 @@
 /*
-Pattern 1:-Right_Angle_Triangle_with_Dots.
-*
-* *
-* * *
-* * * *
+Pattern 1:-Reverse_Right_Angle_Triangle_with_Dots.
 * * * * *
+* * * *
+* * *
+* *
+*
 
 */
 
 import java.util.Scanner;
 
-class Right_Angle_Triangle_with_Dots
+class Reverse_Right_Angle_Triangle_with_Dots
 {
     public static void main (String[] args)
     {
@@ -18,8 +18,10 @@ class Right_Angle_Triangle_with_Dots
         System.out.println("Enter the size:- ");
         int T = sc.nextInt();
         System.out.println("Output is:- ");
-        for(int i=1; i<=T; i++){
-            for(int j=1; j<=i; j++){
+        
+        for(int i=1; i<=T; i++)
+        {
+            for(int j=T; j>=i; j--){
                 System.out.print("* ");
             }
             System.out.println();
