758bfd06b6b3ee7f4bbc3104f238238185af360d
DnyaneshKshatriya07 <dnyaneshkshatriya7123@gmail.com>
2023-12-21 11:40:24 +0530

    Equilateral_Triangle_with_Dots

--- Equilateral_Triangle_with_Dots.java
+++ Equilateral_Triangle_with_Dots.java
@@ -1,30 +1,34 @@
 /*
-Pattern 1:-Reverse_Left_Right_Angle_Triangle_with_Dots.
+Pattern 1:-Equilateral_Triangle_with_Dots.
+    *
+   * *
+  * * *
+ * * * *
 * * * * *
-* * * *
-* * *
-* *
-*
 
 */
 
 import java.util.Scanner;
 
-class Reverse_Left_Right_Angle_Triangle_with_Dots
+class Equilateral_Triangle_with_Dots
 {
     public static void main (String[] args)
     {
+        int sp=0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size:- ");
         int T = sc.nextInt();
         System.out.println("Output is:- ");
-        
-        for(int i=1; i<=T; i++)
-        {
-            for(int j=T; j>=i; j--){
+        sp=T-1;
+        for(int i=1; i<=T; i++){
+            for(int spc=sp; spc>=1; spc--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
                 System.out.print("* ");
             }
             System.out.println();
+            sp--;
         }
     }
 }
