5d4d5e4050fb2d36a36e4e6b9734c9128725b657
DnyaneshKshatriya07 <dnyaneshkshatriya7123@gmail.com>
2023-12-28 12:07:20 +0530

    Right_Right_Angle_Triangle_with_Dots

---
+++ Right_Right_Angle_Triangle_with_Dots.java
@@ -0,0 +1,31 @@
+/*
+Pattern 1:-Right_Right_Angle_Triangle_with_Dots.
+        *
+      * *
+    * * *
+  * * * *
+* * * * * 
+
+*/
+
+import java.util.Scanner;
+
+class Right_Right_Angle_Triangle_with_Dots
+{
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size:- ");
+        int T = sc.nextInt();
+        System.out.println("Output is:- ");
+        for(int i=1; i<=T; i++){
+            for(int j=1;j<=T-i;j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
