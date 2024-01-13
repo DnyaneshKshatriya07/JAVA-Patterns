e40e01c2a0c2544b5e11466be39a91d97d13145b
DnyaneshKshatriya07 <dnyaneshkshatriya7123@gmail.com>
2024-01-13 16:30:04 +0530

    Hollow_Diamond

---
+++ Hollow_Diamond.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Hollow_Diamond {
+    public static void main(String[] args) {
+        int i, j;
+        System.out.println("Enter the size:- ");
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        System.out.println("Output is:- ");
+
+        for (i = 1; i <= T; i++) {
+            for (j = T; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            System.out.print("*");
+
+            for (j = 1; j < (i - 1) * 2; j++) {
+                System.out.print(" ");
+            }
+
+            if (i == 1) {
+                System.out.println();
+            } else {
+                System.out.println("*");
+            }
+        }
+
+        for (i = T-1; i >= 1; i--) {
+            for (j = T; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            System.out.print("*");
+
+            for (j = 1; j < (i - 1) * 2; j++) {
+                System.out.print(" ");
+            }
+
+            if (i == 1) {
+                System.out.println();
+            } else {
+                System.out.println("*");
+            }
+        }
+    }
+}
